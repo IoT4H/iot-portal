36cddf7762b9d9161c212cc5703311f049febb01
Rickmann, Bastian <bastian.rickmann@accenture.com>
2023-05-10 20:17:18 +0200

    feature: adjust header styling

--- frontend/src/app/common/Header.tsx
+++ frontend/src/app/common/Header.tsx
@@ -1,9 +1,8 @@
 'use client';
 import * as React from "react";
-import { Fragment, useState } from 'react'
-import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
 import {
-    ArrowPathIcon,
     Bars3Icon,
     XMarkIcon,
 } from '@heroicons/react/24/outline'
@@ -28,23 +27,29 @@ function classNames(...classes: any[]) {
     return classes.filter(Boolean).join(' ')
 }
 
+function Logo() {
+    return (
+            <a href="/" className="-m-1.5 p-1.5 flex">
+                <span className="sr-only">IoT4H Portal</span>
+                <img className="h-8 w-auto mr-4" src="/iot4h_logo_cropped_normal.png" alt="" />
+                <h1 className="text-black dark:text-white text-3xl font-bold border-l-2 pl-4">Portal</h1>
+            </a>
+    );
+}
+
 export default function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
         <header className="sticky top-0 z-40">
-            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 border-solid border-b border-gray-500/10 dark:border-amber-50/10" aria-label="Global">
+            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 border-solid border-b border-gray-400/10 dark:border-gray-50/30" aria-label="Global">
                 <div className="flex lg:flex-1">
-                    <a href="/" className="-m-1.5 p-1.5 flex">
-                        <span className="sr-only">IoT4H Portal</span>
-                        <img className="h-8 w-auto mr-4" src="/iot4h_logo_cropped_normal.png" alt="" />
-                        <h1 className="text-black dark:text-white text-3xl font-bold border-l-2 pl-4">Portal</h1>
-                    </a>
-                </div>
+                <Logo />
+            </div>
                 <div className="flex lg:hidden">
                     <button
                         type="button"
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 dark:text-amber-50"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 dark:text-orange-50"
                         onClick={() => setMobileMenuOpen(true)}
                     >
                         <span className="sr-only">Open main menu</span>
@@ -56,7 +61,7 @@ export default function Header() {
                         links.map(link =>
                             <a key={link.name}
                                 href={ link.href }
-                                className="text-sm font-semibold leading-6 text-gray-900 dark:text-amber-50"
+                                className="text-sm font-semibold leading-6 text-gray-900 dark:text-white"
                             >
                                 { link.name }
                             </a>
@@ -64,22 +69,16 @@ export default function Header() {
                     }
                 </div>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <a href={'/login'} className="text-sm font-semibold leading-6 text-gray-900 dark:text-amber-50">
+                    <a href={'/login'} className="text-sm font-semibold leading-6 text-gray-900 dark:text-white">
                         Log in <span aria-hidden="true">&rarr;</span>
                     </a>
                 </div>
             </nav>
             <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-                <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white dark:bg-black dark:text-amber-50 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 dark:sm:ring-amber-50/10">
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white dark:bg-black dark:text-orange-50 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 dark:sm:ring-gray-50/10">
+
                     <div className="flex items-center justify-between">
-                        <a href="/" className="-m-1.5 p-1.5">
-                            <span className="sr-only">IoT4H Portal</span>
-                            <img
-                                className="h-8 w-auto"
-                                src="/iot4h_logo_cropped_normal.png"
-                                alt=""
-                            />
-                        </a>
+                        <Logo />
                         <button
                             type="button"
                             className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -90,13 +89,13 @@ export default function Header() {
                         </button>
                     </div>
                     <div className="mt-6 flow-root">
-                        <div className="-my-6 divide-y divide-gray-500/10 dark:divide-amber-50/10">
-                            <div className="space-y-2 py-6">
+                        <div className="-my-6 divide-y divide-gray-400/10 dark:divide-gray-50/30">
+                            <div className="space-y-2 py-3">
                                 {
                                     links.map(link => (
                                         <a  key={link.name}
                                             href={ link.href }
-                                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-amber-100/50 dark:text-amber-50 hover:dark:bg-amber-100/20"
+                                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-orange-400/20 dark:text-white"
                                         >
                                             { link.name }
                                         </a>)
@@ -106,7 +105,7 @@ export default function Header() {
                             <div className="py-6">
                                 <a
                                     href={'/login'}
-                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900  hover:bg-amber-100/50 dark:text-amber-50 hover:dark:bg-amber-100/20"
+                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-orange-400/20 dark:text-white"
                                 >
                                     Log in
                                 </a>
