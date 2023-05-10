e9ee757a7faa7daad28557b775f1224b09875859
Rickmann, Bastian <bastian.rickmann@accenture.com>
2023-05-10 20:15:02 +0200

    feature: translate to German

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -2,21 +2,16 @@ export default function Login() {
     return (
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        className="mx-auto h-10 w-auto"
-                        src="/iot4h_logo_cropped_normal.png"
-                        alt="IoT4H"
-                    />
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-amber-50">
-                        Sign in to your account
+                    <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-orange-50">
+                        Login
                     </h2>
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form className="space-y-6" action="#" method="POST">
                         <div>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900 dark:text-amber-50">
-                                Email address
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900 dark:text-orange-50">
+                                E-Mail
                             </label>
                             <div className="mt-2">
                                 <input
@@ -25,18 +20,18 @@ export default function Login() {
                                     type="email"
                                     autoComplete="email"
                                     required
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-amber-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
 
                         <div>
                             <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900 dark:text-amber-50">
-                                    Password
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900 dark:text-orange-50">
+                                    Passwort
                                 </label>
                                 <div className="text-sm">
-                                    <a href="#" className="font-semibold text-amber-600 hover:text-amber-500">
+                                    <a href="#" className="font-semibold text-orange-400 hover:text-orange-500">
                                         Passwort vergessen?
                                     </a>
                                 </div>
@@ -48,7 +43,7 @@ export default function Login() {
                                     type="password"
                                     autoComplete="current-password"
                                     required
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-amber-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -56,7 +51,7 @@ export default function Login() {
                         <div>
                             <button
                                 type="submit"
-                                className="flex w-full justify-center rounded-md bg-amber-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600"
+                                className="flex w-full justify-center rounded-md bg-orange-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
                             >
                                 Einloggen
                             </button>
@@ -65,8 +60,8 @@ export default function Login() {
 
                     <p className="mt-10 text-center text-sm text-gray-500">
                         Noch kein Account?{' '}
-                        <a href="#" className="font-semibold leading-6 text-amber-600 hover:text-amber-500">
-                            Jetzt einen Anlegen
+                        <a href="#" className="font-semibold leading-6 text-orange-400 hover:text-orange-500">
+                            Jetzt registrieren
                         </a>
                     </p>
                 </div>
