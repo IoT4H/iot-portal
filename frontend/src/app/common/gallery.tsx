37e46e5a54565145ad6d8f61db3f70a9cba19f05
Rickmann, Bastian <bastian.rickmann@accenture.com>
2023-06-07 18:22:30 +0200

    feature: adds gallery and pic viewer

--- frontend/src/app/common/gallery.tsx
+++ frontend/src/app/common/gallery.tsx
@@ -11,10 +11,11 @@ export default function Gallery({ index, pics} : { index: number; pics: any[]}) 
             {
                 pics.length >= 1 && (
                     <div className={"absolute w-full h-full z-50 flex content-center items-center justify-center bg-gray-50/20 "}>
-                        <div className={"bg-zinc-700 p-12 drop-shadow-xl pt-16"}>
-                            <XMarkIcon className={"w-12 absolute top-2 right-2 cursor-pointer"}
-                                        onClick={ () => gallery(0, [])}
-                            />
+                        <div className={"bg-zinc-700 p-12 drop-shadow-2xl pt-16"}>
+                            <div className={"w-12 absolute top-2 right-2 cursor-pointer"}
+                                 onClick={ () => gallery(0, [])}>
+                                <XMarkIcon />
+                            </div>
                             <img src={"http://localhost:1337" + pics[index].url}  className={"max-w-[50vw] max-h-[50vh]"}/>
                         </div>
                         <div className={"absolute w-full h-full"}
