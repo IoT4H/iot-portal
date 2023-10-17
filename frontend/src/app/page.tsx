9f991cfaed899a25d9cd3e16f00018e26855ea45
Rickmann, Bastian <bastian.rickmann@accenture.com>
2023-10-17 18:05:56 +0200

    feature: go to home button improved

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function LandingPage() {
     <main className="flex h-full flex-col items-center justify-between p-24">
 
       <h1>Willkommen</h1>
-        <h2>Go to <Link href={'/home'}>Home</Link></h2>
+        <h2><Link href={'/home'} className={'p-2 px-4 rounded hover:bg-orange-500/20 cursor-pointer'}>Go to <b>Home</b></Link></h2>
 
     </main>
   )
