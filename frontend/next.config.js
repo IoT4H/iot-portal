64f7fc286dd43d4cce25ffc3cd5fbb3c13ee2e50
Rickmann, Bastian <bastian.rickmann@accenture.com>
2024-03-25 16:21:57 +0100

    mod to not using global /api anymore

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    trailingSlash: true,
-    async rewrites() {
-        return [
-            {
-                source: "/api/:path*",
-                destination: `${process.env.STRAPI_API_URL}/:path*`
-            },
-        ];
-    },
+    trailingSlash: true
 }
 
 module.exports = nextConfig
