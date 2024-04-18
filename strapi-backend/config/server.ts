5c5dc1dac96cb33f123da913fa018a3f99a12df9
Rickmann, Bastian <bastian.rickmann@accenture.com>
2024-04-18 17:23:24 +0200

    fix(backend): use environment to point to correct platform

--- strapi-backend/config/server.ts
+++ strapi-backend/config/server.ts
@@ -2,6 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env("STRAPI_URL","/"),
+  thingsboardUrl:  env("THINGSBOARD_URL","http://localhost:9090"),
   app: {
     keys: env.array('APP_KEYS'),
   },
