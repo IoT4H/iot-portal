9fe9964f76aae6dea09b71d820b7ab0a65c87071
Rickmann, Bastian <bastian.rickmann@accenture.com>
2024-08-05 13:09:52 +0200

    fix markdown styling

--- frontend/src/app/common/CustomMarkdown.tsx
+++ frontend/src/app/common/CustomMarkdown.tsx
@@ -3,7 +3,7 @@ import ReactMarkdown from "react-markdown";
 
 export default function CustomMarkdown({children, className, ...rest} : { children: string, className: string }) {
 
-    return <ReactMarkdown className={className} components={
+    return <ReactMarkdown className={`${className} *:mb-[1.5rem] *:last:mb-0`} components={
         {
             h1: "h2",
             h2: "h3",
@@ -22,7 +22,10 @@ export default function CustomMarkdown({children, className, ...rest} : { childr
                 return  <GalleryImage  {...rest} src={rest.src} />;
             },
             p(props) {
-                return <p className={"mb-[1.5rem]"}>{props.children}</p>;
+                return <p className={"mb-[1.5rem] last:mb-0"}>{props.children}</p>;
+            },
+            blockquote(props) {
+                return <blockquote className={"p-2 border-l-4 border-orange-500 mb-[1.5rem] last:mb-0"}>{props.children}</blockquote>;
             }
         }
     } {...rest} >{ children }</ReactMarkdown>
