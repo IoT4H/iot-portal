0d8ee78cdba80662d02673ca8d9ad25aee82d1bd
Rickmann, Bastian <bastian.rickmann@accenture.com>
2024-09-10 18:45:42 +0200

    debugging cred

--- strapi-backend/src/plugins/thingsboard-plugin/server/register.ts
+++ strapi-backend/src/plugins/thingsboard-plugin/server/register.ts
@@ -2,6 +2,11 @@ import { Strapi } from '@strapi/strapi';
 import pluginId from "../admin/src/pluginId";
 
 export default ({ strapi }: { strapi: Strapi }) => {
+
+  console.log(`Connected to ThingsBoard via: ${strapi.plugin(pluginId).config('thingsboardUrl')}`);
+  console.log(`Connected to ThingsBoard via User: ${strapi.plugin(pluginId).config('thingsboardSysAdminUsername').toString().replace(/(?<=.).(?=[^@]*?.@)|(?:(?<=@.)|(?!^)\G(?=[^@]*$))(.)(?=.*\.)|(?:(?<=\.)(?=[^.]+$)|(?!^)\G(?=[^@.]*$))[^.](?!$)/gm, '*')}`);
+  console.log(`Connected to ThingsBoard via Password: ${strapi.plugin(pluginId).config('thingsboardUrl').toString().replace(/.(?=.{4})/gm, "*")}`);
+
   // register phase
   strapi.customFields.register({
     name: "thingsboardUserId",
