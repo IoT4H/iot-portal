618d41c1b6f66383974382e089610ddf6635d82e
Rickmann, Bastian <bastian.rickmann@accenture.com>
2024-09-26 17:16:36 +0200

    ui: correct littlefs api url env key

--- frontend/src/lib/urls.ts
+++ frontend/src/lib/urls.ts
@@ -1,6 +1,6 @@
 export function getUrls() {
     const url = process.env.FRONTEND_STRAPI_API_URL || '';
     const serverUrl = process.env.SERVER_STRAPI_API_URL || process.env.FRONTEND_STRAPI_API_URL || '';
-    const littlefsUrl = process.env.LITTLEFS_URL || 'http://localhost:3001/littlefs.bin';
+    const littlefsUrl = process.env.LITTLEFS_API_URL || 'http://localhost:3001/littlefs.bin';
     return { StrapiURL: url, serverStrapiUrl: serverUrl, littlefsUrl: littlefsUrl };
 }
