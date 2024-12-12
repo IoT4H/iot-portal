225a3079954b027b80cef7e6ae8019291ec0fd3d
Rickmann, Bastian <bastian.rickmann@accenture.com>
2024-12-12 11:27:28 +0100

    feature: makes describing inputs optional and more understandable for the user

--- frontend/src/app/@setup/usecase/[id]/setup/page.tsx
+++ frontend/src/app/@setup/usecase/[id]/setup/page.tsx
@@ -79,21 +79,22 @@ export default function Start({params}: { params: { id: number } }) {
         <ModalUI key={pathname}>
             {
                 user ? <div className={"max-w-3xl w-[75vw]"}>
-                    <h1 className={"w-full mr-8 text-2xl font-bold"}>Setup Einrichten</h1>
+                    <h1 className={"w-full mr-8 text-3xl font-bold"}>Anwendungsfall einrichten</h1>
                     <form onSubmit={(e) =>{ e.preventDefault(); setupStart(); }}>
-                        <label htmlFor={"title"} className={"block text-xl mt-8 flex flex-row items-center gap-1"}>Title <InformationCircleIcon title={"Titel um das Setup wieder zu erkennen"} className={"inline-block align-center h-4 w-4"}></InformationCircleIcon></label>
+                        <label htmlFor={"title"} className={"block text-xl mt-8 flex flex-row items-center gap-1"} title={"Titel um das Setup wiederzuerkennen"}>Title <InformationCircleIcon  className={"inline-block align-center h-4 w-4"}></InformationCircleIcon></label>
                         <input id={"title"} name="title"
                                className={"mt-2 bg-stone-800/10 rounded-md border-black/10 border-1 w-full text-white focus:ring-white focus:border-black/10 "} value={title} onChange={(event)=> { setTitle(event.target.value);}} autoFocus />
-                        <label htmlFor={"description"} className={"block text-xl mt-8 flex flex-row items-center gap-1"}>Beschreibung <InformationCircleIcon title={"Kurze Beschreibung für den Verwendungszweck"} className={"inline-block align-center h-4 w-4"}></InformationCircleIcon></label>
+                        <label htmlFor={"description"} className={"block text-xl mt-8 flex flex-row items-center gap-1"} title={"Diese Beschreibung kann weiteren Aufschluss über Ihren eigenen Nutzen bzw. Vorteil erläutern. z.B. 'Aufzeichnung der Position unserer Bohrer (aus Halle A), welche ständig verlegt werden.'"}>Beschreibung (Optional) <InformationCircleIcon className={"inline-block align-center h-4 w-4"}></InformationCircleIcon></label>
                         <textarea name="description"
+                                  placeholder={"Diese Beschreibung kann weiteren Aufschluss über Ihren eigenen Nutzen bzw. Vorteil erläutern. z.B. 'Aufzeichnung der Position unserer Bohrer (aus Halle A), welche ständig verlegt werden.'"}
                                   className={"mt-2 bg-stone-800/10 rounded-md border-black/10 border-1 w-full text-white focus:ring-white focus:border-black/10 h-64 resize-none"} value={description} onChange={(event)=> { setDescription(event.target.value);}}></textarea>
                         <button
-                            className={"mt-4 cursor-pointer w-full text-center rounded-md ml-auto bg-orange-500/80 hover:bg-orange-500 text-white flex-row flex gap-4 justify-center items-center uppercase mx-4 px-4 py-4"}>Starten
+                            className={"mt-4 cursor-pointer w-full text-center rounded-md ml-auto bg-orange-500/80 hover:bg-orange-500 text-white flex-row flex gap-4 justify-center items-center uppercase mx-4 px-4 py-4"}>Einrichten
                         </button>
                     </form>
                 </div> : <Prompt type={PromptType.Warning} title={"Anmeldung erforderlich"} text={"Um diese Funktion nutzen zu können müssen Sie sich zuerst einloggen."} actions={[{text: "Zurück", actionFunction: () => {router.back()}},{text: "Login", actionFunction: () => router.push("/login")}]} onClose={() => {}} />
             }
-            {hiddenLink}
+            { hiddenLink }
         </ModalUI>
     )
 }
