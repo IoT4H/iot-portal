de3cf4b787c35dc961ed0ecea428cae5f3333431
Rickmann, Bastian <bastian.rickmann@accenture.com>
2025-01-24 10:17:35 +0100

    feature: add a pattern input fieldset

--- frontend/src/app/common/baseLayout.tsx
+++ frontend/src/app/common/baseLayout.tsx
@@ -7,7 +7,7 @@ export default function BaseLayout(props: any) {
             <div className="bg-orange-100 dark:bg-zinc-800 sticky top-16 h-16 z-10">
                 <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8 h-full flex flex-row items-center gap-x-2">
                     {
-                        props.links && props.links.map((link: {  href: string, title: string, deactivated: Function }) => {
+                        props.links && props.links.length > 0 && props.links.map((link: {  href: string, title: string, deactivated: Function }) => {
                                 return (<BaseLayoutLink key={link.href} {...link} />);
                             }
                         )
