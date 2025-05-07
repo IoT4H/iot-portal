15357eb30ae5db059312a431bccbd472f85cbfa5
Krause, Justin <justin.krause@umlaut.com>
2025-05-07 11:43:07 +0200

    chore: exec precommit for auth folder

--- frontend/src/app/(auth)/confirmation/page.tsx
+++ frontend/src/app/(auth)/confirmation/page.tsx
@@ -1,45 +1,40 @@
-"use client"
-import Button from "@iot-portal/frontend/app/common/button";
-import { FieldSetCheckbox, FieldSetInput, RequiredStar } from "@iot-portal/frontend/app/common/FieldSet";
+"use client";
 import { LoadingState } from "@iot-portal/frontend/app/common/pageBlockingSpinner";
 import { Prompt, PromptType } from "@iot-portal/frontend/app/common/prompt";
 import { fetchAPI } from "@iot-portal/frontend/lib/api";
 import { Auth } from "@iot-portal/frontend/lib/auth";
 import Link from "next/link";
-import { useParams, useRouter, useSearchParams } from "next/navigation";
-import { useCallback, useMemo, useState } from "react";
-
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback, useState } from "react";
 
 export default function Page() {
-
     const router = useRouter();
-    const searchParams = useSearchParams()
+    const searchParams = useSearchParams();
 
-    const token = searchParams.get('code');
-
+    const token = searchParams.get("code");
 
     const [error, SetError] = useState<any>(undefined);
 
-    const [success, SetSuccess] = useState<boolean | undefined>(undefined);
-
-    if(!token) {
-        router.push("/login")
+    if (!token) {
+        router.push("/login");
     }
 
     const changePassword = useCallback(() => {
-
-        if(token) {
+        if (token) {
             LoadingState.startLoading();
-            fetchAPI("/api/auth/email-confirmation", {}, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    "code": token,
-                }),
-            }).then(response => {
-
+            fetchAPI(
+                "/api/auth/email-confirmation",
+                {},
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        code: token
+                    })
+                }
+            ).then((response) => {
                 if (response.error) {
                     LoadingState.endLoading();
                 }
@@ -53,18 +48,43 @@ export default function Page() {
         }
     }, [token]);
 
-    return <div
-        className={"relative w-full max-w-screen-lg flex flex-col justify-center items-center flex-wrap top-0 left-0"}>
-        <form className="flex-auto max-h-full w-full" onSubmit={(event) => {event.preventDefault();changePassword();}}>
-            <h2 className={"dark:text-white font-bold text-3xl border-solid border-b-4 inline-block mb-2 pr-2 border-orange-500"}>Passwort vergessen</h2>
-            <div className={" flex flex-col gap-y-8 w-full max-w-xl mx-auto"}>
-                <p className={"my-4"}>Email wurde erfolgreich bestätigt.</p>
-            </div>
-            <p>
-                <Link href={"/usecase"}> Jetzt Anwendungsfälle anschauen.</Link>
-            </p>
+    return (
+        <div
+            className={
+                "relative w-full max-w-screen-lg flex flex-col justify-center items-center flex-wrap top-0 left-0"
+            }
+        >
+            <form
+                className="flex-auto max-h-full w-full"
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    changePassword();
+                }}
+            >
+                <h2
+                    className={
+                        "dark:text-white font-bold text-3xl border-solid border-b-4 inline-block mb-2 pr-2 border-orange-500"
+                    }
+                >
+                    Passwort vergessen
+                </h2>
+                <div className={" flex flex-col gap-y-8 w-full max-w-xl mx-auto"}>
+                    <p className={"my-4"}>Email wurde erfolgreich bestätigt.</p>
+                </div>
+                <p>
+                    <Link href={"/usecase"}> Jetzt Anwendungsfälle anschauen.</Link>
+                </p>
 
-            { error?.message != undefined && <Prompt type={PromptType.Warning} title={"Fehler"} text={error?.message} actions={[{text: "Erneut Versuchen", actionFunction: () => {}}]} onClose={() => SetError(undefined)}/> }
-        </form>
-    </div>;
+                {error?.message != undefined && (
+                    <Prompt
+                        type={PromptType.Warning}
+                        title={"Fehler"}
+                        text={error?.message}
+                        actions={[{ text: "Erneut Versuchen", actionFunction: () => {} }]}
+                        onClose={() => SetError(undefined)}
+                    />
+                )}
+            </form>
+        </div>
+    );
 }
