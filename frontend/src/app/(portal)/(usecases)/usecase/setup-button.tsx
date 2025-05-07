daeed0dd3bca82667586a11a151a5de0c7848186
Krause, Justin <justin.krause@umlaut.com>
2025-05-07 14:03:57 +0200

    chore: exec precommit for portal folder

--- frontend/src/app/(portal)/(usecases)/usecase/setup-button.tsx
+++ frontend/src/app/(portal)/(usecases)/usecase/setup-button.tsx
@@ -1,29 +1,29 @@
-"use client"
+"use client";
 
-
-import { BeakerIcon, PlayIcon } from "@heroicons/react/24/solid";
+import { BeakerIcon } from "@heroicons/react/24/solid";
 import { AuthContext } from "@iot-portal/frontend/app/common/AuthContext";
 import Link from "next/link";
 import { useContext, useEffect, useState } from "react";
 
-export default function SetupButton({ slug } : { slug: string}) {
-
-
-
-
-    const [link, SetLink] = useState<string>(`/usecase/${slug}/setup/`);
+export default function SetupButton({ slug }: { slug: string }) {
+    const [link] = useState<string>(`/usecase/${slug}/setup/`);
     const user = useContext(AuthContext);
 
-    const [isAuth, SetIsAuth] = useState<boolean>(false);
+    const [, SetIsAuth] = useState<boolean>(false);
 
     useEffect(() => {
         SetIsAuth(user !== undefined);
-    }, [user])
-
+    }, [user]);
 
     return link ? (
-        <Link href={link} className={` mt-4 cursor-pointer w-full text-center ml-auto bg-orange-500/80  text-white flex-row flex gap-4 justify-center items-center uppercase mx-4 px-4 py-4 ${ user == undefined ? 'bg-gray-500' : 'hover:bg-orange-500'}`}>
-            <BeakerIcon className={"h-6"}/>Anwendungsfall ausprobieren
+        <Link
+            href={link}
+            className={` mt-4 cursor-pointer w-full text-center ml-auto bg-orange-500/80  text-white flex-row flex gap-4 justify-center items-center uppercase mx-4 px-4 py-4 ${
+                user == undefined ? "bg-gray-500" : "hover:bg-orange-500"
+            }`}
+        >
+            <BeakerIcon className={"h-6"} />
+            Anwendungsfall ausprobieren
         </Link>
     ) : null;
 }
