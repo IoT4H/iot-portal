c7f5d0f8982e84063d4176fdcb65b718b0035088
Rickert, Luis <luis.rickert@accenture.com>
2025-05-14 16:46:08 +0200

    chore: add slugMap to UseCasePage

--- frontend/src/app/(portal)/(usecases)/usecase/[id]/page.tsx
+++ frontend/src/app/(portal)/(usecases)/usecase/[id]/page.tsx
@@ -1,6 +1,6 @@
 import CustomMarkdown from "@iot-portal/frontend/app/common/CustomMarkdown";
 import GalleryImage from "@iot-portal/frontend/app/common/galleryImage";
-import { mapUseCase } from "@iot-portal/frontend/app/common/mappingFunctions";
+import { generateSlugToLinkMap, mapUseCase } from "@iot-portal/frontend/app/common/mappingFunctions";
 import { fetchAPI, getStrapiURLForFrontend } from "@iot-portal/frontend/lib/api";
 import * as React from "react";
 
@@ -22,26 +22,26 @@ function PictureGallery({ pictures } : {pictures?: any[]}) {
 }
 
 export const dynamic = 'force-dynamic';
-export default async function UseCasePage({params}: { params: { id: number } }) {
+export default async function UseCasePage({ params }: { params: { id: number } }) {
 
 
-    const qsPara =
-        {
-            fields: [ 'description' ],
-            populate: {
-                pictures: {
-                    populate: '*',
-                },
-                partnerLogos: {
-                    populate: '*',
-                }
-            },
-            filters: {
-                slug: {
-                    $eq: params.id,
-                },
-            },
-        }
+  const qsPara =
+  {
+    fields: ['description'],
+    populate: {
+      pictures: {
+        populate: '*',
+      },
+      partnerLogos: {
+        populate: '*',
+      }
+    },
+    filters: {
+      slug: {
+        $eq: params.id,
+      },
+    },
+  }
     ;
 
     const useCase = await fetchAPI('/api/use-cases', qsPara).then((data) => {
@@ -49,24 +49,29 @@ export default async function UseCasePage({params}: { params: { id: number } }) 
     });
 
 
-    return (<>
-        <Info description={useCase.description}/>
-        <div className={"mt-8 mx-8 flex flex-col gap-8"}>
-            <PictureGallery pictures={useCase.pictures}/>
-            { Array.isArray(useCase.partnerLogos) && useCase.partnerLogos.length > 0 &&
-                <div className={"rounded  text-black"}>
-                     <h3 className="dark:text-white font-bold text-xl  border-solid border-b-[0.2em] inline-block pr-[0.5em] py-1 border-orange-500 pb-[1px]">Vorgestellt durch:</h3>
-                    <div className="flex flex-row gap-4 flex-wrap w-full flex-grow-0 mt-4">
-                        {
-                            useCase.partnerLogos.map((pL) => (
-                                /* eslint-disable-next-line @next/next/no-img-element */
-                                <GalleryImage className={"w-40 object-center object-contain py-2 px-4 bg-white rounded "} key={pL.hash}
-                                     src={getStrapiURLForFrontend(pL.formats?.small?.url || pL.url)}
-                                     alt={pL.alternativeText}/>
-                            ))
-                        }
-                    </div>
-                </div>}
-        </div>
-    </>);
+
+
+
+
+
+  return (<>
+    <Info description={useCase.description} />
+    <div className={"mt-8 mx-8 flex flex-col gap-8"}>
+      <PictureGallery pictures={useCase.pictures} />
+      {Array.isArray(useCase.partnerLogos) && useCase.partnerLogos.length > 0 &&
+        <div className={"rounded  text-black"}>
+          <h3 className="dark:text-white font-bold text-xl  border-solid border-b-[0.2em] inline-block pr-[0.5em] py-1 border-orange-500 pb-[1px]">Vorgestellt durch:</h3>
+          <div className="flex flex-row gap-4 flex-wrap w-full flex-grow-0 mt-4">
+            {
+              useCase.partnerLogos.map((pL) => (
+                /* eslint-disable-next-line @next/next/no-img-element */
+                <GalleryImage className={"w-40 object-center object-contain py-2 px-4 bg-white rounded "} key={pL.hash}
+                  src={getStrapiURLForFrontend(pL.formats?.small?.url || pL.url)}
+                  alt={pL.alternativeText} />
+              ))
+            }
+          </div>
+        </div>}
+    </div>
+  </>);
 }
