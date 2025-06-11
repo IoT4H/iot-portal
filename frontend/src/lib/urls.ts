bab96fb899c8bd1b49c0bcf12b9283c2e55c8586
Rickmann, Bastian <bastian.rickmann@accenture.com>
2025-06-11 15:49:46 +0200

    feature: add wifi credentials and server details to config

--- frontend/src/lib/urls.ts
+++ frontend/src/lib/urls.ts
@@ -1,6 +1,16 @@
-export function getUrls() {
+export type URLS = {
+    StrapiURL: string
+    serverStrapiUrl: string
+    littlefsUrl: string
+    platformUrl: string
+    dashboardUrl: string
+}
+
+export function getUrls() : URLS  {
     const url = process.env.FRONTEND_STRAPI_API_URL || '';
     const serverUrl = process.env.SERVER_STRAPI_API_URL || process.env.FRONTEND_STRAPI_API_URL || '';
     const littlefsUrl = process.env.LITTLEFS_API_URL || 'http://localhost:3001/littlefs.bin';
-    return { StrapiURL: url, serverStrapiUrl: serverUrl, littlefsUrl: littlefsUrl };
+    const platformUrl = process.env.PLATFORM_URL || 'http://localhost:9090';
+    const dashboardUrl = process.env.DASHBOARD_URL || 'http://localhost:9090';
+    return { StrapiURL: url, serverStrapiUrl: serverUrl, littlefsUrl: littlefsUrl, platformUrl: platformUrl, dashboardUrl: dashboardUrl };
 }
