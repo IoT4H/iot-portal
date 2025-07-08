9615f43a35f803e3e1cb5bc3f76f0cbadc800ed8
Rickmann, Bastian <bastian.rickmann@accenture.com>
2025-07-08 20:49:06 +0200

    feat(frontend): makes wifi credentials optional

--- frontend/src/app/common/FieldSet.tsx
+++ frontend/src/app/common/FieldSet.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
 
 import { CheckIcon } from "@heroicons/react/24/solid";
-import { createRef, useEffect, useReducer, useRef, useState } from "react";
 import * as React from "react";
+import { createRef, useEffect, useReducer, useRef, useState } from "react";
 
 export function RequiredStar (p: any) {
 
@@ -23,8 +23,10 @@ export function FieldSetInput(p: any) {
             </label>
             <div className="mt-2">
                 { props.multiline ?
-                    <textarea name={props.id} {...props} className="block h-[5em] resize-none w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6"></textarea> :
-                    <input name={props.id} {...props} className=" flex-grow block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6" />
+                  <textarea name={props.id} {...props}
+                            className=" block h-[5em] resize-none w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6 disabled:bg-gray-500 disabled:ring-gray-800"></textarea> :
+                  <input name={props.id} {...props}
+                         className=" flex-grow block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6 disabled:bg-gray-500 disabled:ring-gray-800" />
                 }
             </div>
                 { children && ( Array.isArray(children ) &&  children.map((child: any) => {
@@ -44,7 +46,8 @@ export function FieldSetSelect(p: any) {
             { props.label && ( <label htmlFor={props.id} className="block font-bold text-base leading-7 text-gray-900 dark:text-orange-50">
                 {props.label}{props.required && (<RequiredStar />)}
             </label> ) }
-            <select className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6" {...props}>
+          <select
+            className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6  disabled:bg-gray-500 disabled:ring-gray-800" {...props}>
                 { Array.isArray(children ) &&  children.map((child: any) => {
                     return child;
                 }) || children }
@@ -62,15 +65,16 @@ export function FieldSetCheckbox(p: any) {
             <div className={`relative block group/checkbox ${props.className || ""}`}>
                 <label htmlFor={props.id}
                     className={`relative flex gap-2 flex-row items-center p-0.5 group/checkbox group-[:not(:has(*:disabled))]/checkbox:cursor-pointer`}>
-                                        <span
+                    <span
                                             className={`h-6 aspect-square border-2 border-orange-500 bg-orange-500/20 group-[:has(*:checked)]/checkbox:bg-orange-500 group-[:not(:has(*:disabled))]/checkbox:group-hover/checkbox:bg-orange-500/40 `}>
+
                                             <CheckIcon className={`w-full aspect-square invisible group-[:has(*:checked)]/checkbox:visible`}/>
                                         </span>
                     <input type={"checkbox"} {...props} className={"hidden"} />
                     <div>
-                        { Array.isArray(children ) &&  children.map((child: any) => {
+                      {props.label || Array.isArray(children) && children.map((child: any) => {
                             return child;
-                        }) || children }
+                      }) || children}
                         {props.required && (<RequiredStar />)}</div>
                 </label>
             </div>
