e7137932ea13c3e12a13ad48f70025eb9416851f
Rickmann, Bastian <bastian.rickmann@accenture.com>
2025-07-21 20:27:21 +0200

    feature: expand endpoint with attributes

--- strapi-backend/src/plugins/thingsboard-plugin/server/services/strapi-service.ts
+++ strapi-backend/src/plugins/thingsboard-plugin/server/services/strapi-service.ts
@@ -246,40 +246,47 @@ export default ({ strapi }: { strapi: Strapi }) => ({
     return profiles;
   },
   async getInstructionStepsFromDeployment(deploymentId: number) {
-    const deployment: any = await strapi.entityService.findOne("api::deployment.deployment", deploymentId, {
-      populate: {
-        use_case: {
-          populate: {
-            setupSteps: {
-              populate: {
-                meta: {
-                  populate: "*"
-                },
-                tasks: {
-                  populate: "*"
-                },
-                relations: {
-                  populate: "*"
-                },
-                flashConfig: {
-                  populate: "*"
-                },
-                flashInstruction: {
-                  populate: {
-                    binary: {
-                      populate: "*"
-                    }
+    const deployment: any = await strapi.entityService.findOne(
+      "api::deployment.deployment",
+      deploymentId,
+      {
+        populate: {
+          use_case: {
+            populate: {
+              setupSteps: {
+                populate: {
+                  meta: {
+                    populate: "*"
+                  },
+                  tasks: {
+                    populate: "*"
+                  },
+                  relations: {
+                    populate: "*"
+                  },
+                  flashConfig: {
+                    populate: "*"
+                  },
+                  flashInstruction: {
+                    populate: {
+                      binary: {
+                        populate: "*"
+                      },
+                    },
+                  },
+                  alternativeLabel: {
+                    populate: "*"
+                  },
+                  serverAttributes: {
+                    populate: "*"
                   }
                 },
-                alternativeLabel: {
-                  populate: "*"
-                }
-              }
-            }
-          }
-        }
+              },
+            },
+          },
+        },
       }
-    });
+    );
 
     const cMs = deployment.use_case.setupSteps.map((step) => step.__component === "instructions.setup-instruction" ? this.replaceUUIDsForDeployment(deployment.deployed, step) : step);
 
