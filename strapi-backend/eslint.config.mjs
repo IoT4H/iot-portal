8270ccab2bda0586fe3960fc382f8409dbaf5f66
Krause, Justin <justin.krause@umlaut.com>
2025-07-30 17:09:08 +0200

    chore: add strapi lint and prettier

---
+++ strapi-backend/eslint.config.mjs
@@ -0,0 +1,37 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import prettier from "eslint-plugin-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{ts,tsx,js,jsx,mjs,cjs}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        ecmaVersion: "latest",
+        sourceType: "module"
+      },
+      globals: globals.node
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      prettier
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      "prettier/prettier": "error"
+    }
+  },
+
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"]
+  }
+]);
